97cb778de7f407df3495c6b8b0f9a4704a63cdc4
YewonSeo <yewonblairseo@gmail.com>
2022-05-29 14:47:16 +0200

    frontend designing

---
+++ views/components/search-engine.js
@@ -0,0 +1,8 @@
+console.log('search engine js loaded')
+
+
+function displayInfoModal(name, description) {
+    console.log("displayinfo modal clicked")
+    console.log("item name", name)
+    console.log("description ", description)
+}
