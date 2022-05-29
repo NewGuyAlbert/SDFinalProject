a76252bb6621f8c970c79e96421d3ca95d7f9956
YewonSeo <yewonblairseo@gmail.com>
2022-05-29 22:38:42 +0200

    modal display done

--- views/components/search-engine.js
+++ views/components/search-engine.js
@@ -1,8 +1,12 @@
-console.log('search engine js loaded')
 
+function displayInfoModal(id, name) {
+    $("#bg-info-modal-title").html(name)
+    $("#modal-description > span").html($(`#${id}-description`).html())
+    $("#modal-genre > span").html($(`#${id}-genre`).html())
+    $("#modal-noOfPlayers > span").html($(`#${id}-noOfPlayers`).html())
+    $("#modal-duration > span").html($(`#${id}-duration`).html())
+    $("#modal-ageLimit > span").html($(`#${id}-ageLimit`).html())
+    $("#modal-language > span").html($(`#${id}-language`).html())
+    $("#bg-info-modal").modal("show")
 
-function displayInfoModal(name, description) {
-    console.log("displayinfo modal clicked")
-    console.log("item name", name)
-    console.log("description ", description)
 }
