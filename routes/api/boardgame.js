fb0f58df26e857188cec1d05e03c1ae2c7813466
YewonSeo <yewonblairseo@gmail.com>
2022-06-05 21:40:11 +0200

    CRUD for admin boardgame DONE!

--- routes/api/boardgame.js
+++ routes/api/boardgame.js
@@ -38,5 +38,35 @@ router.post("/add-boardgame-item", async (req, res) => {
     }
 })
 
+router.put("/edit-boardgame-item", async (req, res) => {
+    try {
+        let newCondition = req.body.condition
+        if (newCondition == "New" || newCondition == "Good" || newCondition == "Moderate" || newCondition == "Bad") {
+            await BoardgameItem.updateOne({_id: req.body.id}, {$set: {condition: newCondition}})
+            res.status(200).send("Edit Success!")
+        } else {
+            res.status(404).send("Wrong condition.")
+        }
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error)
+    }
+})
+
+router.delete("/delete-boardgame-item", async (req, res) => {
+    try {
+        let item = await BoardgameItem.find({ _id: req.body.id }).select("isAvailable").limit(1)
+        if (item[0].isAvailable) {
+            await BoardgameItem.deleteOne({ _id: req.body.id })
+            res.status(200).send("Delete succeeded.")
+        } else {
+            res.status(404).send("Cannot delete the item. The item is attached to an order.")
+        }
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error)
+    }
+})
+
 
 module.exports = router
--- views/admin/admin-dashboard.js
+++ views/admin/admin-dashboard.js
@@ -11,15 +11,23 @@ function getBoardgames() {
         items.forEach((item, index) => {
             i = index + 1
             $("#boardgame-table-body").append(`
-            <tr>
+            <tr id="tr-${item._id}">
                 <th scope="row">${i}</th>
                 <th>${item._id}</th>
                 <td>${item.boardgameId.name}</td>
+                <td>${item.condition}</td>
                 <td>${item.isAvailable === true ? "Available" : "Not available"}</td>
-                <td><button class="admin-edit-modal-btn" data-toggle="modal" data-target="#admin-bgmodal"><i class="fa-solid fa-pen-to-square"></i></button></td>
-                <td><i class="fa-solid fa-trash"></i></td>
+                <td><button class="admin-edit-modal-btn" data-id="${item._id}"><i class="fa-solid fa-pen-to-square" data-id="${item._id}"></i></button></td>
+                <td class="delete-bgitem"><button class="fa-solid fa-trash" onclick="deleteBoardgame('${item._id}')"></button></td>
             </tr>
             `)
+        })
+
+        $("#boardgame-table-body > tr > td > .admin-edit-modal-btn").on('click', (e) => {
+            e.preventDefault()
+            let id = $(e.target).data('id')
+            $(".edit-save-id").html(id)
+            $("#admin-bgmodal-edit").modal('show')
         })
     }).fail( function(){
         console.log("error")
@@ -44,13 +52,14 @@ function addBoardgame() {
         console.log(response)
         $("#admin-bgmodal").modal('hide')
         $("#boardgame-table-body").append(`
-            <tr>
+            <tr id="tr-${response.newItem._id}">
                 <th scope="row">${i}</th>
                 <th>${response.newItem.boardgameId}</th>
                 <td>${bgTitle}</td>
+                <td>${bgCondition}</td>
                 <td>${response.newItem.isAvailable === true ? "Available" : "Not available"}</td>
-                <td><button class="admin-edit-modal-btn" data-toggle="modal" data-target="#admin-bgmodal"><i class="fa-solid fa-pen-to-square"></i></button></td>
-                <td><i class="fa-solid fa-trash"></i></td>
+                <td><button class="admin-edit-modal-btn" data-id="${response.newItem._id}" ><i class="fa-solid fa-pen-to-square" data-id="${response.newItem._id}"></i></button></td>
+                <td class="delete-bgitem"><button class="fa-solid fa-trash" onclick="deleteBoardgame('${response.newItem._id}')"></button></td>
             </tr>
             `)
     }).fail( function(){
@@ -61,7 +70,53 @@ function addBoardgame() {
 }
 
 function editBoardgame() {
+    let bgCondition = $("#bg-condition-edit").val()
+    let bgID = $(".edit-save-id").html()
+
+    $.ajax({
+        method: "PUT",
+        url: "/edit-boardgame-item",
+        data: { "id": bgID, "condition": bgCondition }
+    }).done( function(response){
+        alert(response)
+        if (response == "Edit Success!") {
+            $("#admin-bgmodal-edit").modal('hide')
+            $(`#tr-${bgID} > td:nth-child(4)`).html(bgCondition)
+        }
+    }).fail( function(error){
+        if (error.responseText) {
+            alert(error.responseText)
+        }
+        console.log("error: ", error)
+    })
 
 }
 
-getBoardgames()
+function deleteBoardgame(id) {
+    $.ajax({
+        method: "DELETE",
+        url: "/delete-boardgame-item",
+        data: { "id" : id }
+    }).done( function(response){
+        if (response == "Delete succeeded.") {
+            $(`#tr-${id}`).remove()
+        }
+        alert(response)
+    }).fail( function(error){
+        console.log("error ", error)
+    })
+}
+
+
+// JQuery
+$(function() {
+    getBoardgames()
+    
+    // $("#boardgame-table-body > tr > td > .admin-edit-modal-btn").on('click', (e) => {
+    //     e.preventDefault()
+    //     console.log("here")
+    //     // console.log($(this))
+    //     // let id = $(this).data('id')
+    //     // console.log('id', id)
+    // })
+})
